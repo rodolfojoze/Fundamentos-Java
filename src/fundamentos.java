c0248cf8bc4ce18aafb47103c44f2c8d587fbf9a
José Rodolfo Carreira de Oliveira <jose.oliveira@db1.com.br>
2025-08-23 18:21:44 -0300

    Primeiro commit, a famosa frase!

---
+++ src/fundamentos.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class fundamentos {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite uma frase:");
+        String frase = input.nextLine();
+
+        System.out.println(frase);
+    }
+
+
+}
